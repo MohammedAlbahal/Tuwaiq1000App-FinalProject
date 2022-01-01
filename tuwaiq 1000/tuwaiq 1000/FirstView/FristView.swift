36530082e2ff2b072ad3956cf2ece1b5fc360460
mohammedali111 <91420952+mohammedali111@users.noreply.github.com>
2022-01-02 00:28:22 +0300

    add a button with alert in first view

--- tuwaiq 1000/tuwaiq 1000/FirstView/FristView.swift
+++ tuwaiq 1000/tuwaiq 1000/FirstView/FristView.swift
@@ -8,7 +8,41 @@
 import UIKit
 
 class FristView: UIViewController , UICollectionViewDelegate, UICollectionViewDataSource , UICollectionViewDelegateFlowLayout {
-    
+    lazy var logOutBtn: UIButton = {
+        let buttonSingOut = UIButton(type: .system)
+        buttonSingOut.setTitle(NSLocalizedString("singOut", comment: ""), for: .normal)
+        buttonSingOut.setTitleColor(.red, for: .normal)
+        buttonSingOut.translatesAutoresizingMaskIntoConstraints = false
+        buttonSingOut.titleLabel?.font = .systemFont(ofSize: 20, weight: .bold)
+        buttonSingOut.addTarget(self, action: #selector(singOutButtonTapped), for: .touchUpInside)
+        return buttonSingOut
+    }()
+    @objc private func singOutButtonTapped(sender: UIButton!) {
+        let alert = UIAlertController(title: "Sign Out",
+                                      message: "Are you sure you want to sign out",
+                                      preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "Yes", style: .default, handler: {
+            action in
+            self.dismiss(animated: true) {
+                self.navigationController?.popViewController(animated: true)
+            }
+            
+        }))
+        alert.addAction(UIAlertAction(title: "No", style: .cancel))
+        self.present(alert, animated: true)
+    }
+    func setupButtonForSignOut() {
+        view.addSubview(logOutBtn)
+        
+        
+        logOutBtn.addTarget(self, action: #selector(singOutButtonTapped), for: .touchUpInside)
+    }
+//    logOutBtn.setTitle("Log Out", for: .normal)
+//    logOutBtn.backgroundColor = UIColor(named: "Color-2")
+//    logOutBtn.titleLabel?.textColor = UIColor(named: "Color-1")
+//    logOutBtn.titleLabel?.tintColor = UIColor(named: "Color-1")
+//    logOutBtn.titleLabel?.font = UIFont(name: "Chalkboard SE", size: 20)
+//    logOutBtn.addTarget(self, action: #selector(singOutButtonTapped), for: .touchUpInside)
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
         let screenSize: CGSize = UIScreen.main.bounds.size
 
@@ -17,6 +51,13 @@ class FristView: UIViewController , UICollectionViewDelegate, UICollectionViewDa
         } else {
             return CGSize(width: screenSize.width/2, height: 200)
         }
+    
+    }
+    func setUpBottomViewConstraints(){
+    logOutBtn.translatesAutoresizingMaskIntoConstraints = false
+    logOutBtn.leadingAnchor.constraint(equalTo: self.view.leadingAnchor, constant: 150).isActive = true
+    logOutBtn.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 100).isActive = true
+    logOutBtn.heightAnchor.constraint(equalToConstant: 20).isActive = true
     
     }
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
@@ -80,7 +121,8 @@ class FristView: UIViewController , UICollectionViewDelegate, UICollectionViewDa
    
         picC.delegate = self
         picC.dataSource = self
-       
+        setupButtonForSignOut()
+        setUpBottomViewConstraints()
     }
    
 }
